e028d15c6db36668723c58a19c362b2a8328fb3c
Michael Lin <mili@yahoo-inc.com>
2017-02-01 11:19:45 -0800

    milestone 4

--- Canvas/ViewController.m
+++ Canvas/ViewController.m
@@ -13,6 +13,9 @@
 @property (nonatomic, assign) CGPoint trayOriginalCenter;
 @property (nonatomic, assign) CGPoint trayCenterWhenOpen;
 @property (nonatomic, assign) CGPoint trayCenterWhenClosed;
+
+@property (nonatomic, strong) UIImageView *newlyCreatedFace;
+@property (nonatomic, assign) CGPoint newlyCreatedFaceOriginalCenter;
 @end
 
 @implementation ViewController
@@ -29,6 +32,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 - (IBAction)onTrayPanGesture:(UIPanGestureRecognizer *)sender {
     // Absolute (x,y) coordinates in parentView
     CGPoint location = [sender locationInView:self.view];
@@ -54,6 +58,34 @@
                 self.trayView.center = self.trayCenterWhenOpen;
             }];
         }
+    }
+}
+- (IBAction)facePan:(UIPanGestureRecognizer *)sender {
+    NSLog(@"face pan");
+    // Absolute (x,y) coordinates in parentView
+    CGPoint location = [sender locationInView:self.view];
+
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        // Gesture recognizers know the view they are attached to
+        UIImageView *imageView = (UIImageView *)sender.view;
+        
+        // Create a new image view that has the same image as the one currently panning
+        self.newlyCreatedFace = [[UIImageView alloc] initWithImage:imageView.image];
+        
+        // Add the new face to the tray's parent view.
+        [self.view addSubview:self.newlyCreatedFace];
+        
+        // Initialize the position of the new face.
+        self.newlyCreatedFace.center = imageView.center;
+        self.newlyCreatedFaceOriginalCenter = self.newlyCreatedFace.center;
+        
+        // Since the original face is in the tray, but the new face is in the
+        // main view, you have to offset the coordinates
+        CGPoint faceCenter = self.newlyCreatedFace.center;
+        self.newlyCreatedFace.center = CGPointMake(faceCenter.x, faceCenter.y + self.trayView.frame.origin.y);
+    } else if (sender.state == UIGestureRecognizerStateChanged) {
+        self.newlyCreatedFace.center = location;
+    } else if (sender.state == UIGestureRecognizerStateEnded) {
     }
 }
 
